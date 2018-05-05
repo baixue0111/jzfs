e6d95c9bd20fb80e480f49d13b4fa3f7baae5ee1
baixue0111 <1187314214@qq.com>
2018-05-05 11:30:05 +0800

    贸促会

--- mch/js/index.js
+++ mch/js/index.js
@@ -2,16 +2,67 @@
 $(function () {
     // 图片轮播
     var mySwiper = new Swiper(".swiper-container", {
-        autoplay: 500,
+        autoplay: 5000,
         loop: true,
-        pagination: {
-            el: ".swiper-pagination"
-        },
+        pagination: ".swiper-pagination",
         paginationType: "bullets",
         paginationClickable : true,
         navigation: {
           nextEl: ".imgNews-swiper-next",
           prevEl: '.imgNews-swiper-prev'
         }
+      })
+
+      // 工作动态 ----  新闻滚动
+      var mySwiper2 = new Swiper(".swiper-container2", {
+        direction: 'vertical',
+        autoplay: 5000,
+        loop: true,
+        paginationType: "bullets",
+        paginationClickable : true
+      })
+
+      // 鼠标经过图标变为“通知公告”
+      // $(".hxgdBgImg").hover(function () {
+      //   $(".hxgdBgImg").html("通知公告")
+      // })
+      //文字横向滚动
+      var num = 0;
+      function goLeft() {
+          //750是根据你给的尺寸，可变的
+          if (num == -750) {
+              num = 0;
+          }
+          num -= 1;
+          $(".hxgdCon").css({
+              left: num
+          })
+      }
+      //设置滚动速度
+      var timer = setInterval(goLeft, 50);
+      //设置鼠标经过时滚动停止
+      $("#scroll-div").hover(function() {
+          clearInterval(timer);
+      },
+      function() {
+          timer = setInterval(goLeft, 20);
+      })
+
+      // 学术论文
+      var mySwiper3 = new Swiper(".swiper-container3", {
+        autoplay: 5000,
+        loop: true,
+        pagination: ".xslwSwiperPage",
+        paginationType: "bullets",
+        paginationClickable : true
+      })
+
+
+      var mySwiper4 = new Swiper(".swiper-container4", {
+        autoplay: 5500,
+        loop: true,
+        pagination: ".xslwSwiperPage",
+        paginationType: "bullets",
+        paginationClickable : true
       })
 })
